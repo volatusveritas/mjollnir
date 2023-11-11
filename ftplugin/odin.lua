1abf547a3ea08d7a0245413d027e560be0dacc6e
Volatus <volatusCoder@outlook.com>
2023-11-10 22:45:16 -0300

    fix: make odin ftplugin commands buffer local

--- ftplugin/odin.lua
+++ ftplugin/odin.lua
@@ -4,33 +4,37 @@ vim.bo.commentstring = "// %s"
 local keymap = require('volt.keymap')
 local odin = require('volt.lang.odin')
 
-keymap.set("n", {
-    ["<LocalLeader>f"] = {
-        desc = "Find procedure",
-        map = function()
-            vim.ui.input({ prompt = "Procedure name: " }, function(input)
-                if input == nil then
+keymap.set("n", {['<LocalLeader>'] = {
+    desc = 'Odin special commands',
+    opts = { buffer = 0 },
+    map = {
+        f = {
+            desc = "Find procedure",
+            map = function()
+                vim.ui.input({ prompt = "Procedure name: " }, function(input)
+                    if input == nil then
+                        return
+                    end
+
+                    vim.cmd(string.format([[/^\w*%s\w*\s*::\s*proc]], input))
+                end)
+            end,
+        },
+        e = {
+            desc = 'Quickfix-check package',
+            map = function()
+                package_name = vim.fn.input({
+                    prompt = '(Odin check quickfix) package name: ',
+                    completion = 'dir',
+                    cancelreturn = nil,
+                })
+
+                if package_name == '' or package_name == nil then
                     return
                 end
 
-                vim.cmd(string.format([[/^\w*%s\w*\s*::\s*proc]], input))
-            end)
-        end,
+                odin.check_package(package_name)
+            end,
+        },
     },
-    ['<LocalLeader>e'] = {
-        desc = 'Quickfix-check package',
-        map = function()
-            package_name = vim.fn.input({
-                prompt = '(Odin check quickfix) package name: ',
-                completion = 'dir',
-                cancelreturn = nil,
-            })
-
-            if package_name == '' or package_name == nil then
-                return
-            end
-
-            odin.check_package(package_name)
-        end,
-    },
-})
+}})
