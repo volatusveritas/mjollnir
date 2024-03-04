1ef6f71b083aae3fe4ca5ab6c150cfb559b383ec
Volatus <volatusCoder@outlook.com>
2024-03-04 10:29:10 -0300

    feat: add Pmenu colors to palette

--- pack/volt/start/volt/lua/palette.lua
+++ pack/volt/start/volt/lua/palette.lua
@@ -42,12 +42,14 @@ function M.apply()
     highlight.set('LineNrBelow', { fg = M.color.grey4 })
     highlight.set('LineNr', { fg = M.color.light4 })
     highlight.set('CurSearch', { fg = M.color.dark1, bg = M.color.yellow })
-    -- highlight.set('NormalFloat', { fg = M.color.light1, bg = M.color.dark1 })
-    -- highlight.set('FloatBorder', { fg = M.color.purple, bg = M.color.dark1 })
     highlight.set('NormalFloat', { fg = M.color.light1, bg = 'NONE' })
     highlight.set('FloatBorder', { fg = M.color.purple, bg = 'NONE' })
     highlight.set('FloatTitle', { fg = M.color.purple, attr = { 'bold' } })
     highlight.set('FloatFooter', { fg = M.color.purple })
+    highlight.set('Pmenu', { fg = M.color.light1, bg = M.color.dark4 })
+    highlight.set('PmenuSel', { fg = M.color.dark1, bg = M.color.purple_dark })
+    highlight.set('PmenuSBar', { fg = M.color.light1, bg = M.color.dark4 })
+    highlight.set('PmenuThumb', { fg = M.color.light1, bg = M.color.purple })
 
     vim.o.foldtext = 'v:lua.volt_foldtext()'
 end
